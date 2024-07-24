82e8054185571bc8d807f7e36be17f4a78d33285
Robbie <robbie331@>
2024-07-24 12:01:22 +0900

    test

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -11,7 +11,7 @@ public class App {
         int num1 = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
-
+//test
         System.out.println("num1,num2= " + num1 + "," + num2);
 
     }
